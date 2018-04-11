83c917054b30a22ffb751132f6b925edd732976e
jimmyrleung <jimmy.rl19@hotmail.com>
2018-04-10 21:35:07 -0300

    Configurando contexto, mapeando entidade e primeiro insert com o entity

---
+++ Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class LojaContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Indicamos o nosso servidor
+            optionsBuilder.UseSqlServer("Server=DESKTOP-440J00T;Database=EFCORE_LojaDB;Trusted_Connection=true;");
+        }
+    }
+}
