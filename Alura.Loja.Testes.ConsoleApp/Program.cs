874dce3af1bbd5966314f2083f7e946f38a67a71
jimmyrleung <jimmy.rl19@hotmail.com>
2018-04-17 21:18:33 -0300

    conhecendo as operações de CRUD

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -10,8 +10,55 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            //GravarUsandoAdoNet();
+            GravarUsandoAdoNet();
             GravarUsandoEntity();
+            RecuperarUsandoEntity();
+            ExcluirUsandoEntity();
+            RecuperarUsandoEntity();
+            AlterarUsandoEntity();
+            System.Threading.Thread.Sleep(10000);
+        }
+
+        private static void ExcluirUsandoEntity()
+        {
+            using (var repo = new LojaContext())
+            {
+                IList<Produto> produtos = repo.Produtos.ToList();
+                foreach (var p in produtos)
+                {
+                    repo.Produtos.Remove(p);
+                }
+                repo.SaveChanges();
+            }
+        }
+
+        private static void AlterarUsandoEntity()
+        {
+            Produto p = new Produto();
+            p.Nome = "Cassino Royalr";
+            p.Categoria = "Filme";
+            p.Preco = 17.99;
+
+            // Adicionando produto
+            using (var contexto = new LojaContext())
+            {
+                contexto.Produtos.Add(p);
+                contexto.SaveChanges();
+            }
+
+            using (var contexto = new LojaContext())
+            {
+                p.Nome = "Cassino Royale";
+                contexto.Produtos.Update(p);
+                contexto.SaveChanges();
+            }
+
+            using (var contexto = new LojaContext())
+            {
+                var produto = contexto.Produtos.Find(p.Id);
+                Console.WriteLine("Nome corrigido: " + p.Nome);
+                contexto.SaveChanges();
+            }
         }
 
         private static void GravarUsandoEntity()
@@ -38,6 +85,25 @@ namespace Alura.Loja.Testes.ConsoleApp
             using (var repo = new ProdutoDAO())
             {
                 repo.Adicionar(p);
+            }
+        }
+
+        private static void RecuperarUsandoEntity()
+        {
+            using (var repo = new LojaContext())
+            {
+                IList<Produto> produtos = repo.Produtos.ToList();
+                if(produtos.Count > 0)
+                {
+                    foreach (var p in produtos)
+                    {
+                        Console.WriteLine(String.Format("Livro: {0} - R$ {1}", p.Nome, p.Preco));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Não existem livros cadastrados.");
+                }
             }
         }
     }
