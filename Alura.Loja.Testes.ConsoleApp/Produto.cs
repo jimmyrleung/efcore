882326a000f02fb7c36e128062292e6aa8ea9f68
jimmyrleung <jimmy.rl19@hotmail.com>
2018-04-18 21:01:16 -0300

    Entendendo o change tracker e os estados

--- Alura.Loja.Testes.ConsoleApp/Produto.cs
+++ Alura.Loja.Testes.ConsoleApp/Produto.cs
@@ -6,5 +6,13 @@
         public string Nome { get; internal set; }
         public string Categoria { get; internal set; }
         public double Preco { get; internal set; }
+
+        public override string ToString()
+        {
+            // return string.Format("Produto: {0} - Categoria: {1} - Preço: R$ {2}", this.Nome, this.Categoria, this.Preco);
+
+            // Interpolação de string sem format
+            return $"Produto: {this.Nome} - Categoria: {this.Categoria} - Preço: R$ {this.Preco}";
+        }
     }
 }
--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,16 +12,75 @@ namespace Alura.Loja.Testes.ConsoleApp
 {
     class Program
     {
+        /* Aula 3 - ChangeTracker e estados */
         static void Main(string[] args)
         {
-            GravarUsandoAdoNet();
-            GravarUsandoEntity();
-            RecuperarUsandoEntity();
-            ExcluirUsandoEntity();
-            RecuperarUsandoEntity();
-            AlterarUsandoEntity();
-            System.Threading.Thread.Sleep(10000);
+            using (var context = new LojaContext())
+            {
+                // Logger
+                var serviceProvider = context.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+                // Busca os produtos
+                IList<Produto> produtos = context.Produtos.ToList();
+
+                // Exibe o estado dos produtos
+                exibeEntriesComState(context.ChangeTracker.Entries().ToList());
+
+                // Altera uma propriedade de um produto
+                var p1 = produtos.First();
+                p1.Nome = "Cassino Royale alterado sem updater";
+
+                // Exibe o estado dos produtos
+                exibeEntriesComState(context.ChangeTracker.Entries().ToList());
+
+                // Desabilitar o autodetect por questões de performance
+                //context.ChangeTracker.AutoDetectChangesEnabled = false;
+
+                // Só pelo fato do produto estar "Modified" o SaveChanges fará um 
+                // update implícito
+                context.SaveChanges();
+
+                // Pega a lista atualizada
+                produtos = context.Produtos.ToList();
+
+                exibeEntriesComState(context.ChangeTracker.Entries().ToList());
+
+                var p2 = new Produto() { Nome = "Adicionado e removido antes do SaveChanges", Categoria = "Outros", Preco = 999.99 };
+
+                // Adicionamos e removemos sem dar um SaveChanges
+                context.Produtos.Add(p2);
+                context.Produtos.Remove(p2);
+
+                // Entidade deve estar Detached
+                var detachedEntry = context.Entry(p2);
+                Console.WriteLine(detachedEntry.Entity.ToString() + " - " + detachedEntry.State);
+
+
+                System.Threading.Thread.Sleep(10000);
+            }
         }
+
+        public static void exibeEntriesComState(IList<EntityEntry> entries)
+        {
+            foreach(var e in entries)
+            {
+                Console.WriteLine(e.Entity.ToString() + " - " + e.State);
+            }
+        }
+
+        /* Aula 1 e 2 - CRUD */
+        //static void Main(string[] args)
+        //{
+        //    GravarUsandoAdoNet();
+        //    GravarUsandoEntity();
+        //    RecuperarUsandoEntity();
+        //    ExcluirUsandoEntity();
+        //    RecuperarUsandoEntity();
+        //    AlterarUsandoEntity();
+        //    System.Threading.Thread.Sleep(10000);
+        //}
 
         private static void ExcluirUsandoEntity()
         {
